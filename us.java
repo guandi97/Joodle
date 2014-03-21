8045b42fd31ca7272b6aa0c3d87d4991d1919faf
Guandi97 <alex@micropenguin.net>
2014-03-21 17:15:19 -0400

    us class constructor

--- us.java
+++ us.java
@@ -3,16 +3,20 @@ import java.util.regex.*;
 
 class us
 {
+	fileIO profile_user;
 	public us(String profile)
 	{
-		
-		fileIO profile_io=new fileIO("./profiles/Profiles");
+		{	
+			fileIO profile_io=new fileIO("./profiles/Profiles");
 
-		String profile=profile_io.readLine();
-		while(profile.matches(^profile)!=true)
-		{
 			String profile=profile_io.readLine();
+			while(profile.matches(^profile)!=true)
+			{
+				String profile=profile_io.readLine();
+			}
 		}
-		
+
+		String profile_arr[]=profile.split("\\s");
+		profile_user=new fileIO("./profiles/"+profile_arr[1]);
 	}
 	public config
