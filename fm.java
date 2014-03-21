05267df05da8dd6d0d255b4fa35124b7699a2b47
Guandi97 <alex@micropenguin.net>
2014-03-21 14:42:46 -0400

    ss class, fm imports

--- fm.java
+++ fm.java
@@ -1,3 +1,6 @@
+import java.nio.file.*;
+import java.io.*;
+
 class fm
 {	
 	Path file
---
+++ us.java
@@ -0,0 +1,14 @@
+import java.io.FILE;
+import java.util.regex.*;
+
+class us
+{
+	public us(String profile)
+	{
+		
+		fileIO profile_io=new fileIO("./profiles/Profiles");
+
+		String profile=profile_io.readLine();
+		
+		
+	}	
