ccd540d57c42a85d36f778825926f1f258158547
Guandi97 <alex@micropenguin.net>
2014-03-21 17:27:53 -0400

    us class: initial profile managing

--- us.java
+++ us.java
@@ -4,19 +4,28 @@ import java.util.regex.*;
 class us
 {
 	fileIO profile_user;
+
 	public us(String profile)
 	{
-		{	
-			fileIO profile_io=new fileIO("./profiles/Profiles");
+		fileIO profile_io=new fileIO("./profiles/Profiles");
 
+		String profile=profile_io.readLine();
+		while(profile.matches(^profile)!=true)
+		{
 			String profile=profile_io.readLine();
-			while(profile.matches(^profile)!=true)
-			{
-				String profile=profile_io.readLine();
-			}
+			if(profile==null) continue;
 		}
-
+		if(profile!=null) profile_exists(profile);
+		else profile_new(profile);
+	}
+	private void profile_exists(String profile)
+	{
 		String profile_arr[]=profile.split("\\s");
 		profile_user=new fileIO("./profiles/"+profile_arr[1]);
 	}
+	private String profile_new(String profile)
+	{
+		
+	}
 	public config
+}
